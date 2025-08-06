7832404054a2449f51662832d2d43a9dd1f303e2
Vinicius <vfarias.designer@gmail.com>
2025-08-05 22:28:38 -0300

    create menu mobile and inicialized hero component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,24 @@
 import * as S from "./styles.js"
 import brandLogo from "../../assets/logo_nav.png"
+import { TfiAlignRight, TfiClose } from "react-icons/tfi";
+import { useState } from "react";
 
 export const Header = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <S.Header>
             <S.NavBar>
                 <S.BrandNav href="#home">
                     <S.Logo src={brandLogo} />
                 </S.BrandNav>
-                <S.Menu>
-                    <S.MenuItem><S.linkItem href="#sobre">Sobre</S.linkItem></S.MenuItem>
-                    <S.MenuItem><S.linkItem href="#servicos">Serviços</S.linkItem></S.MenuItem>
-                    <S.MenuItem><S.linkItem href="#contato">Contato</S.linkItem></S.MenuItem>
+                <S.hamburguer onClick={() => setOpen(!open)}>
+                    {open ? <TfiClose size={30} color="#C6364D" /> : <TfiAlignRight size={30} color="#C6364D" />}
+                </S.hamburguer>
+                <S.Menu open={open}>
+                    <S.MenuItem><S.linkItem onClick={() => setOpen(!open)} href="#sobre">Sobre</S.linkItem></S.MenuItem>
+                    <S.MenuItem><S.linkItem onClick={() => setOpen(!open)} href="#servicos">Serviços</S.linkItem></S.MenuItem>
+                    <S.MenuItem><S.linkItem onClick={() => setOpen(!open)} href="#contato">Contato</S.linkItem></S.MenuItem>
                     <S.MenuItem><S.linkItem className="btnWhatsapp" href="#agendamento">Agende seu Horario!</S.linkItem></S.MenuItem>
                 </S.Menu>
             </S.NavBar>
---
+++ src/components/Hero/styles.js
@@ -0,0 +1,19 @@
+import { styled } from "styled-components";
+
+export const HeroContainer = styled.div`
+`;
+
+export const HeroImage = styled.img`
+`;
+
+export const HeroContent = styled.div`
+`;
+
+export const HeroTitle = styled.h1`
+`;
+
+export const HeroDescription = styled.p`
+`;
+
+export const HeroButton = styled.button`
+`;
